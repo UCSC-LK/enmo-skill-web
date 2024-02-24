9667c58dcace3a75162fc46fd681dcf1206cbbbc
Sajith091” <sajithbandara091@gmail.com>
2024-02-24 20:53:28 +0530

    edit UI

--- Javascripts/ticketListView.js
+++ Javascripts/ticketListView.js
@@ -15,9 +15,13 @@ function getCookie(cookieName) {
 const url = new URL(window.location.href);
 var ticketID = url.searchParams.get('ticketID');
 
+
 const PopupPerent = document.querySelector(".body-main")
 const PopupChild = document.querySelector(".body")
 const PopupChild2 = document.querySelector(".body2")
+const loding = document.querySelector(".loading");
+
+loding.style.display ="none"
 
 var myHeaders = new Headers();                          
 myHeaders.append("Content-Type", "application/json");  
@@ -32,57 +36,85 @@ var requestOptions = {
     redirect: 'follow'
     };
 
-    fetch(BASE_URL+"/support?TicketId="+encodeURIComponent(ticketID), requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        result.forEach(element => {
-            document.querySelector(".subject").textContent = element.subject;
-            document.querySelector(".description").textContent = element.description;
-            document.querySelector(".date").textContent = element.date;
-            // document.querySelector(".header").textContent = element.date;
+loding.style.display ="flex"
+fetch(BASE_URL+"/support?TicketId="+encodeURIComponent(ticketID), requestOptions)
+  .then(response => {
+    loding.style.display ="none"
+    if(response.status == 401){
+      window.location.href = "../Failed/401.html";
+    }else if(response.status == 406){
+      const currentUrl = encodeURIComponent(window.location.href);
+      window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+    }else if(response.status == 404){
+      window.location.href = "../Failed/404.html";
+    }else {
+      return response.json()
+    }
+  })
+  .then(result => {
+    console.log(result)
+    result.forEach(element => {
+      document.querySelector(".subject").textContent = element.subject;
+      document.querySelector(".description").textContent = element.description;
+      document.querySelector(".date").textContent = element.date;
 
-            var status = element.status
+      // document.querySelector(".header").textContent = element.date;
 
-            switch(status){
-                
-                case 1:
-                  status="ONGOING"
-                  document.querySelector(".header").textContent=status
-                  document.querySelector(".header-status").style.backgroundColor= "yellow" //rgba(234, 234, 0, 0.486)
-                  break
-      
-                case 2:
-                  status="ONGOING"
-                  document.querySelector(".header").textContent=status
-                  document.querySelector(".header-status").style.backgroundColor=  "yellow"//rgba(234, 234, 0, 0.486)
-                  break
-      
-                  case 3:
-                    status="CLOSED"
-                    document.querySelector(".header").textContent=status
-                    document.querySelector(".header-status").style.backgroundColor= "green"//rgba(0, 232, 28, 0.678)
-                    break
+      var status = element.status
 
-                  case 4:
-                    status="REJECTED"
-                    document.querySelector(".header").textContent=status
-                    document.querySelector(".header-status").style.backgroundColor= "red"//rgba(255, 1, 1, 0.492)
-                    break
-      
-              }
-        })
-        
+      setpackage(element.packages)// to display packege img-----------------------------------------
+   
+
+      switch(status){
+          
+        case 1:
+          status="Ongoing"
+          document.querySelector(".header").textContent=status
+          document.querySelector(".header").style.backgroundColor= "rgba(234, 234, 0, 0.486)" 
+        break
+
+        case 2:
+          status="Ongoing"
+          document.querySelector(".header").textContent=status
+          document.querySelector(".header").style.backgroundColor=  "rgba(234, 234, 0, 0.486)"
+          break
+
+        case 3:
+          status="Closed"
+          document.querySelector(".header").textContent=status
+          document.querySelector(".header").style.backgroundColor= "rgba(0, 232, 28, 0.678)" 
+          break
+
+        case 4:
+          status="Rejected"
+          document.querySelector(".header").textContent=status
+          document.querySelector(".header").style.backgroundColor= "rgba(255, 1, 1, 0.492)"
+          break
+        }
     })
-    .catch(error => console.log('error', error));
+      
+  })
+.catch(error => console.log('error', error));
 
-    document.querySelector(".HistoryBTN").addEventListener("click",()=>{ 
-        getHistroy(ticketID)
-       })
+//call getHistroy() for show ticket updates---------------------------------------------------------
+document.querySelector(".HistoryBTN").addEventListener("click",()=>{ 
+  getHistroy(ticketID)
+})
+
+//is packege issue-----------------------------------------------------------------------------------
+function setpackage(ispackage){
+  if(ispackage>0){
+    console.log(ispackage)
+    document.querySelector(".column").style.display = "flex"
+    document.querySelector(".body-colum").classList.add("body-colum1")
+  }
+}
+
 
 function  getHistroy(ticketID){
 
     document.querySelector(".HistoryBTN").remove();
-
+    
     var myHeaders = new Headers();                          
     myHeaders.append("Content-Type", "application/json");  
     myHeaders.append("Authorization", getCookie("JWT"));   
@@ -96,11 +128,24 @@ function  getHistroy(ticketID){
     redirect: 'follow'
     };
 
-
+    loding.style.display ="flex"
     fetch(BASE_URL+"/support?popup="+encodeURIComponent(ticketID), requestOptions)
-    .then(response => response.json())
+    .then(response => {
+      loding.style.display ="none"
+      if(response.status == 401){
+        window.location.href = "../Failed/401.html";
+      }else if(response.status == 406){
+        const currentUrl = encodeURIComponent(window.location.href);
+        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+      }else if(response.status == 404){
+        window.location.href = "../Failed/404.html";
+      }else {
+        return response.json()
+      }
+    })
     .then(result => {
-    console.log(result)
+    
+    if(result.length>0){document.querySelector(".bottom-header").style.display="flex"}
     result.forEach(element => {
         const newItem = PopupChild2.cloneNode(true)
         //  newItem.querySelector(".subject").textContent=element.subject;
