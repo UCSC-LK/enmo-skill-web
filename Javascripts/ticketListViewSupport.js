8e540c29756c446821d4b4ef3efe62658eecfd3c
Sajith091” <sajithbandara091@gmail.com>
2024-02-25 23:03:53 +0530

    edit UI and set comment and update to support ticket view

--- Javascripts/ticketListViewSupport.js
+++ Javascripts/ticketListViewSupport.js
@@ -17,11 +17,12 @@ const url = new URL(window.location.href)
 var ticketID = url.searchParams.get('ticketID')
 var assigned = url.searchParams.get('assigned')
 
-const PopupPerent = document.querySelector(".body-main")
 const PopupChild = document.querySelector(".body")
+const PopupChild2 = document.querySelector(".body2")
 const loding = document.querySelector(".loading");
 
 loding.style.display ="none"
+
 
 var myHeaders = new Headers();                          
 myHeaders.append("Content-Type", "application/json");  
@@ -31,111 +32,157 @@ var raw = JSON.stringify({});
 
 
 var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-    };
+method: 'GET',
+headers: myHeaders,
+redirect: 'follow'
+};
 
-    loding.style.display ="flex"
-    fetch(BASE_URL+"/support?TicketId="+encodeURIComponent(ticketID), requestOptions)
-    .then(response => {
-      loding.style.display ="none"
-      if(response.status == 401){
-        window.location.href = "../Failed/401.html";
-      }else if(response.status == 406){
-        const currentUrl = encodeURIComponent(window.location.href);
-        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
-      }else if(response.status == 404){
-        window.location.href = "../Failed/404.html";
-      }else {
-        return response.json()
-      }
-    })
-    .then(result => {
-        result.forEach(element => {
-            document.querySelector(".profile-pic").src = element.url;
-            document.querySelector(".header").textContent = element.userName;
-            document.querySelector(".email").textContent = element.email;
-            document.querySelector(".subject").textContent = element.subject;
-            document.querySelector(".description").textContent = element.description;
-            document.querySelector(".date").textContent = element.date;
-            // document.querySelector(".header").textContent = element.date;
-
-        })
-        
-    })
-    .catch(error => console.log('error', error));
-
-    document.querySelector(".HistoryBTN").addEventListener("click",()=>{ 
-        getHistroy(ticketID)
-       })
-
-function  getHistroy(ticketID){
-
-    document.querySelector(".HistoryBTN").remove();
-    
-
-    var myHeaders = new Headers();                          
-    myHeaders.append("Content-Type", "application/json");  
-    myHeaders.append("Authorization", getCookie("JWT"));   
-
-    var raw = JSON.stringify({});
-
-
-    var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-    };
-
-    loding.style.display ="flex"
-    fetch(BASE_URL+"/support?popup="+encodeURIComponent(ticketID), requestOptions)
-    .then(response => {
-      loding.style.display ="none"
-      if(response.status == 401){
-        window.location.href = "../Failed/401.html";
-      }else if(response.status == 406){
-        const currentUrl = encodeURIComponent(window.location.href);
-        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
-      }else if(response.status == 404){
-        window.location.href = "../Failed/404.html";
-      }else {
-        return response.json()
-      }
-    })
-    .then(result => {
-    console.log(result)
+loding.style.display ="flex"
+fetch(BASE_URL+"/support?TicketId="+encodeURIComponent(ticketID), requestOptions)
+.then(response => {
+  loding.style.display ="none"
+  if(response.status == 401){
+    window.location.href = "../Failed/401.html";
+  }else if(response.status == 406){
+    const currentUrl = encodeURIComponent(window.location.href);
+    window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+  }else if(response.status == 404){
+    window.location.href = "../Failed/404.html";
+  }else {
+    return response.json()
+  }
+})
+.then(result => {
     result.forEach(element => {
-        const newItem = PopupChild.cloneNode(true)
-        newItem.querySelector(".subject").textContent=element.subject;
-        newItem.querySelector(".description").textContent=element.description;
-        newItem.querySelector(".date").textContent=element.date;
+        document.querySelector(".profile-pic").src = element.url;
+        document.querySelector(".header").textContent = element.userName;
+        document.querySelector(".email").textContent = element.email;
+        document.querySelector(".subject").textContent = element.subject;
+        document.querySelector(".description").textContent = element.description;
+        document.querySelector(".date").textContent = element.date;
+        // document.querySelector(".header").textContent = element.date;
 
-        PopupPerent.appendChild(newItem)
-    });
-    
+      if(element.order){
+        document.querySelector(".urgent").style.display="inline"
+        document.querySelector(".refund").style.display="inline"
+      }else if(element.urgent){
+        document.querySelector(".urgent").style.display="inline"
+      }
 
-        const btns = document.querySelector(".btns")
-        PopupPerent.appendChild(btns)
-
-        var cloase = document.querySelector(".closeBTN")
-        var reject = document.querySelector(".rejectBTN")
-        var desition = null;
-
-        cloase.addEventListener("click",()=>{
-            desition="solved"
-            viewrequest(ticketID,desition)
-        })
-
-        reject.addEventListener("click",()=>{
-            desition="reject"
-            viewrequest(ticketID,desition)
-        })
-        
+        setpackage(element.packages)// to display packege img-----------------------------------------
 
     })
-    .catch(error => console.log('error', error));
+    
+})
+.catch(error => console.log('error', error));
+
+document.querySelector(".HistoryBTN").addEventListener("click",()=>{ 
+
+  document.querySelector(".HistoryBTN").remove();
+
+  var massage = "Updates"
+  viewMore(ticketID,massage)//get updates-----------------------------------------
+
+  var massage = "Comments"
+  viewMore(ticketID,massage)//get comments-----------------------------------------
+ 
+
+})
+
+function  viewMore(ticketID,massage){
+
+  if(massage == "Updates"){
+    var pvalue = "popup" 
+    var PopupPerent = document.querySelector(".body-main") 
+
+  }else if(massage == "Comments"){
+    var pvalue = "comment" 
+     var PopupPerent = document.querySelector(".c-append") 
+  }
+
+  
+  var myHeaders = new Headers();                          
+  myHeaders.append("Content-Type", "application/json");  
+  myHeaders.append("Authorization", getCookie("JWT"));   
+
+
+  var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+  };
+ 
+  loding.style.display ="flex"
+  fetch(BASE_URL+"/support?"+encodeURIComponent(pvalue)+"="+encodeURIComponent(ticketID), requestOptions)
+  .then(response => {
+    loding.style.display ="none"
+    if(response.status == 401){
+      window.location.href = "../Failed/401.html";
+    }else if(response.status == 406){
+      const currentUrl = encodeURIComponent(window.location.href);
+      window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+    }else if(response.status == 404){
+      window.location.href = "../Failed/404.html";
+    }else {
+      return response.json()
+    }
+  })
+  .then(result => {
+  
+  if(result.length > 0 && massage == "Updates"){document.querySelector(".bottom-header").style.display="flex"}
+  else if(result.length > 0 && massage == "Comments"){document.querySelector(".bottom-header2").style.display="flex"}
+  
+  result.forEach((element, index) => {
+      const newItem = PopupChild2.cloneNode(true)
+      //  newItem.querySelector(".subject").textContent=element.subject;
+
+      newItem.querySelector(".description2").textContent=element.description;
+      newItem.querySelector(".date2").textContent=element.date;
+
+    if (index % 2 === 0) {
+        newItem.classList.add("even-item");
+    } else {
+        newItem.classList.add("odd-item");
+    }
+
+      PopupPerent.appendChild(newItem)
+  })
+      
+
+  const btns = document.querySelector(".btns")
+  document.querySelector(".c-append").appendChild(btns)
+
+  var cloase = document.querySelector(".closeBTN")
+  var reject = document.querySelector(".rejectBTN")
+  var comment = document.querySelector(".commentBTN")
+  var desition = null;
+
+  cloase.addEventListener("click",()=>{
+      desition="solved"
+      viewrequest(ticketID,desition)
+  })
+
+  reject.addEventListener("click",()=>{
+    desition="reject"
+    viewrequest(ticketID,desition)
+  })
+
+  comment.addEventListener("click",()=>{
+    viewrequest2(ticketID)
+  })
+      
+
+  })
+  .catch(error => console.log('error', error));
 } 
+
+function setpackage(ispackage){
+  if(ispackage>0){
+    console.log(ispackage)
+    document.querySelector(".column").style.display = "flex"
+    document.querySelector(".body-colum").classList.add("body-colum1")
+  }
+}
 
 
 
@@ -163,7 +210,62 @@ function viewrequest(TicketID,desition){
   var no = document.querySelector(".no")
 
   yes.addEventListener("click",()=>{
-    viewrequest2(TicketID,desition)
+    
+  var myHeaders = new Headers();                          
+  myHeaders.append("Content-Type", "application/json");  
+  myHeaders.append("Authorization", getCookie("JWT"));  
+
+
+  var requestOptions = {
+    method: 'OPTIONS',
+    headers: myHeaders,
+    redirect: 'follow',
+  };
+
+  if(desition=="solved"){
+
+    loding.style.display ="flex" 
+    fetch(BASE_URL+"/support?Decision=Clos&TicketId="+encodeURIComponent(TicketID), requestOptions)
+    .then(response => {
+      loding.style.display ="none"
+      if(response.status == 401){
+        window.location.href = "../Failed/401.html";
+      }else if(response.status == 406){
+        const currentUrl = encodeURIComponent(window.location.href);
+        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+      }else if(response.status == 404){
+        window.location.href = "../Failed/404.html";
+      }else {
+        return response.json()
+      }
+    })
+    .then(result => {alert(result)
+        window.location="../HTML/ticketListCS.html"})
+    .catch(error => console.log('error', error));
+
+  }else if(desition=="reject"){
+    
+    loding.style.display ="flex"
+    fetch(BASE_URL+"/support?Decision=Reject&TicketId="+encodeURIComponent(TicketID), requestOptions)
+    .then(response => {
+      loding.style.display ="none"
+      if(response.status == 401){
+        window.location.href = "../Failed/401.html";
+      }else if(response.status == 406){
+        const currentUrl = encodeURIComponent(window.location.href);
+        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+      }else if(response.status == 404){
+        window.location.href = "../Failed/404.html";
+      }else {
+        return response.json()
+      }
+    })
+    .then(result => {alert(result)
+      window.location="../HTML/ticketListCS.html"
+    })
+    .catch(error => console.log('error', error));
+
+    }
   })
 
   no.addEventListener("click",()=>{
@@ -172,85 +274,65 @@ function viewrequest(TicketID,desition){
 }
 
 // add comment to ticket-------------------------------------------------------------------------------------
-function viewrequest2(TicketID,desition){
-    let popup_con=document.querySelector(".pop-up-container2");
-    let popup_details=document.querySelector(".pop-up2");
+function viewrequest2(TicketID){
+  let popup_con=document.querySelector(".pop-up-container2");
+  let popup_details=document.querySelector(".pop-up2");
 
-    massege = "Add a comment "
+  massege = "Add a comment "
 
-    popup_con.style.display="flex";
-    popup_details.style.display="inline";
+  popup_con.style.display="flex";
+  popup_details.style.display="inline";
 
-    popup_details.querySelector(".massege").textContent = massege;
+  popup_details.querySelector(".massege").textContent = massege;
 
-    var submit = document.querySelector(".submitBTN")
-    var cancel = document.querySelector(".cancelBTN")
-    
+  var submit = document.querySelector(".submitBTN")
+  var cancel = document.querySelector(".cancelBTN")
+  
 
-    submit.addEventListener("click",()=>{
+  submit.addEventListener("click",()=>{
 
-        var myHeaders = new Headers();                          
-        myHeaders.append("Content-Type", "application/json");  
-        myHeaders.append("Authorization", getCookie("JWT"));  
+    var comment = document.getElementById("description").value
+    console.log(comment)
 
-        var raw = JSON.stringify({
-            "description":document.getElementById("description").value      
-        });
-    
-    
-        var requestOptions = {
-            method: 'OPTIONS',
-            headers: myHeaders,
-            redirect: 'follow',
-            body: raw
-          };
-    
-        if(desition=="solved"){
-          loding.style.display ="flex" 
-            fetch(BASE_URL+"/support?Decision=Clos&TicketId="+encodeURIComponent(TicketID), requestOptions)
-            .then(response => {
-              loding.style.display ="none"
-              if(response.status == 401){
-                window.location.href = "../Failed/401.html";
-              }else if(response.status == 406){
-                const currentUrl = encodeURIComponent(window.location.href);
-                window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
-              }else if(response.status == 404){
-                window.location.href = "../Failed/404.html";
-              }else {
-                return response.json()
-              }
-            })
-            .then(result => {alert(result)
-                window.location="../HTML/ticketListCS.html"})
-            .catch(error => console.log('error', error));
-    
-        }else if(desition=="reject"){
-          loding.style.display ="flex"
-            fetch(BASE_URL+"/support?Decision=Reject&TicketId="+encodeURIComponent(TicketID), requestOptions)
-            .then(response => {
-              loding.style.display ="none"
-              if(response.status == 401){
-                window.location.href = "../Failed/401.html";
-              }else if(response.status == 406){
-                const currentUrl = encodeURIComponent(window.location.href);
-                window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
-              }else if(response.status == 404){
-                window.location.href = "../Failed/404.html";
-              }else {
-                return response.json()
-              }
-            })
-            .then(result => {alert(result)
-                window.location="../HTML/ticketListCS.html"})
-            .catch(error => console.log('error', error));
-    
-        }
-      })
+    var myHeaders = new Headers();                          
+    myHeaders.append("Content-Type", "application/json");  
+    myHeaders.append("Authorization", getCookie("JWT"));
 
-      cancel.addEventListener("click",()=>{
-        location.reload()
-      })
+    var raw = JSON.stringify({
+      "description": comment
+    });
+  
+    var requestOptions = {
+      method: 'OPTIONS',
+      headers: myHeaders,
+      redirect: 'follow',
+      body: raw
+    };
+
+    loding.style.display ="flex"
+    fetch(BASE_URL+"/support?TicketId="+encodeURIComponent(TicketID), requestOptions)
+    .then(response => {
+      loding.style.display ="none"
+      if(response.status == 401){
+        window.location.href = "../Failed/401.html";
+      }else if(response.status == 406){
+        const currentUrl = encodeURIComponent(window.location.href);
+        window.location.href = "../Failed/Session%20timeout.html?returnUrl="+currentUrl;
+      }else if(response.status == 404){
+        window.location.href = "../Failed/404.html";
+      }else {
+        return response.text()
+      }
+    })
+    .then(result => {alert(result)
+      location.reload();})
+    .catch(error => console.log('error', error));
+
+    })
+  
+    cancel.addEventListener("click",()=>{
+      location.reload()
+    })
     
 }
 
