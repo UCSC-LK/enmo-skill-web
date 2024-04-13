eaf7d2e87fb99e527f0edaeba2fc2f6bfde2b820
Nirasha Nelki <nirashanelki@gmail.com>
2024-04-13 12:56:42 +0530

    some updates in the css

--- Javascripts/policy_violations.js
+++ Javascripts/policy_violations.js
@@ -10,10 +10,26 @@ document.getElementById("overlay1_close").addEventListener("click", function(){
 })
 
 document.getElementById("bann").addEventListener("click", function(){
-    overlay2.style.display = "block"
+    Swal.fire({
+        title: "Are you sure?",
+        text: "Do you want to Bann this user, userId: " + "userId",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Bann user!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "User was banned",
+            // text: "Your file has been deleted.",
+            icon: "success"
+          });
+        }
+      });
 });
 
-document.getElementById("overlay2_close").addEventListener("click", function(){
-    overlay2.style.display = "none";
-})
+// document.getElementById("overlay2_close").addEventListener("click", function(){
+//     overlay2.style.display = "none";
+// })
 
